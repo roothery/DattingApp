da62f782e623db32eef0e3840da501647cec45c3
roothery <roothery@gmail.com>
2023-06-05 10:21:09 -0400

    Updating resources

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { Observable, map } from 'rxjs';
-import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { map, Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private accountService: AccountService, private toastr: ToastrService) {
-  }
+    constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
-  canActivate(): Observable<boolean> {
-    return this.accountService.currentUser$.pipe(
-      map(user => {
-        if (user) return true;
-        else {
-          this.toastr.error('You shall not pass!')
-          return false;
-        }
-      })
-    );
-  }
-
+    canActivate(): Observable<boolean> {
+        return this.accountService.currentUser$.pipe(
+            map(user => {
+                if (user) return true;
+                else {
+                    this.toastr.error('You shall not pass!');
+                    return false;
+                }
+            })
+        );
+    }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     NotFoundComponent,
     ServerErrorComponent,
     TestErrorComponent,
-    MemberCardComponent
+    MemberCardComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -46,7 +49,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
